786427b562f60226879462415b554ec1800e3af7
Noriyuki Takishita <n_taki@taki-lab.site>
2018-11-04 11:22:03 +0900

    ファーストコミット

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MacroDB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
